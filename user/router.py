7a2e089bef4969e26bb8a97e95ee251ed1532813
Chanchalmalviya <you@example.com>
2024-01-13 13:07:52 +0530

    index

--- user/router.py
+++ user/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Request,Form
+from fastapi import APIRouter,Request,Form,status
 from .models import *
 from fastapi.responses import HTMLResponse,RedirectResponse
 from .pydantic_models import Persone,deleteuser,getdata,updateuser
@@ -24,4 +24,37 @@ async def read_iteam(request:Request):
                                       {"request":request}
                                       )
 
+@router.post("/ragistration/",response_class=HTMLResponse)
+async def read_item(request:Request,full_name: str = Form(...),
+                    Email: str = Form(...),
+                    Phone: str = Form(...),
+                    Password: str = Form(...)):
+    if await User.filter(email=Email).exists():
+       
+        return RedirectResponse("/",status_code=status.HTTP_302_FOUND)
+    
+    elif await User.filter(phone=Phone).exists():
+      
+        return RedirectResponse("/",status_code=status.HTTP_302_FOUND)
+    
+    else:
+        await User.create(email=Email,
+                          name=full_name,
+                          phone=Phone,
+                          password=get_password_hash(Password))
+        return RedirectResponse("/login/",status_code=status.HTTP_302_FOUND)
+    
+@router.get("/login/",response_class=HTMLResponse)
+async def read_item(request:Request):
+    return templates.TemplateResponse("login.html",{
+        "request":request,
+    })
+
+@router.get("/table/",response_class=HTMLResponse)
+async def read_item(request:Request):
+    person = await User.all()
+    return templates.TemplateResponse("table.html",{
+        "request":request,
+        "person":person
+    })
 
